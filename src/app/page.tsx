46251d83c272aca5a54b2997dfe9b6644689b1c1
Franco Armando <francoarmando1911@gmail.com>
2025-08-11 17:23:01 -0300

    update slider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection"; 
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Slider/>
       <main>
         <HomeSection />
       </main>
