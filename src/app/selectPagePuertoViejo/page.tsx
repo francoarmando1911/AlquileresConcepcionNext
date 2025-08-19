2da9014ac4f5c298268dbb734a4c8ce505226505
Franco Armando <francoarmando1911@gmail.com>
2025-08-19 12:09:11 -0300

    featured select house page zona ciervo

--- src/app/selectPagePuertoViejo/page.tsx
+++ src/app/selectPagePuertoViejo/page.tsx
@@ -1,12 +1,16 @@
 import Header from '@/components/Header'
 import React from 'react'
+import Footer from '@/components/Footer'
 import PropertiesPuertoViejo from './SelectHousePagePuertoViejo'
 
 export default function page() {
   return (
-    <>
-    <Header/>
-    <PropertiesPuertoViejo/>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <PropertiesPuertoViejo />
+      </main>
+      <Footer />
+    </div>
   )
 }
