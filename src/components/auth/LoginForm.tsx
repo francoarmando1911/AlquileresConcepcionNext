b8928eaa0cc96b1272264151408e90d62625c0cf
Franco Armando <francoarmando1911@gmail.com>
2025-09-05 10:17:16 -0300

    update

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -25,11 +25,13 @@ export default function LoginForm({ onSwitchToRegister, onClose }: LoginFormProp
             const response = await authService.login(formData);
             localStorage.setItem('token', response.token);
             localStorage.setItem('user', JSON.stringify(response.user));
-
+            onClose();
             window.location.reload();
         } catch (err: unknown) {
-            if(err instanceof Error){
+            if (err instanceof Error) {
                 setError(err.message);
+            } else {
+                setError('Error desconocido al iniciar sesión');
             }
         } finally {
             setLoading(false);
